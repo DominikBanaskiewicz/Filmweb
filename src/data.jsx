52df0e81667ab941f6caef1f806ccb885124e502
Dominik Banaskiewicz <banaskiewiczdominik@gmail.com>
2023-07-02 22:10:25 +0200

    Small changes

--- src/data.jsx
+++ src/data.jsx
@@ -38,21 +38,19 @@ export const projectsDatas = [
   },
   {
     id: nanoid(),
-    name: 'Images',
-    url: 'https://github.com/DominikBanaskiewicz/goit-react-hw-04-images-1',
-    demo_url:
-      'https://dominikbanaskiewicz.github.io/goit-react-hw-04-images-1/',
-    technologies: ['javascript', 'restApi', 'react', 'css', 'scrum', 'sass'],
+    name: 'Mongose, node.js',
+    url: 'https://github.com/DominikBanaskiewicz/hw03-mongodb-1',
+    demo_url: 'https://github.com/DominikBanaskiewicz/hw03-mongodb-1',
+    technologies: ['MongDB', 'Express', 'npm', 'mongoose', 'morgan'],
     descriptin:
-      'Me and five other peoples write this site. Crating this site i have learned how to use git with others people, how to choose the best way and how to work with someone else code',
+      'One of the first project in Express, node.js. I learned how to connect to MongoDb threw mongosse, how to create rest Api',
   },
   {
     id: nanoid(),
-    name: 'Images',
-    url: 'https://github.com/DominikBanaskiewicz/goit-react-hw-04-images-1',
-    demo_url:
-      'https://dominikbanaskiewicz.github.io/goit-react-hw-04-images-1/',
-    technologies: ['javascript', 'restApi', 'react', 'css', 'scrum', 'sass'],
+    name: 'Images finder in vanilla js',
+    url: 'https://github.com/DominikBanaskiewicz/goit-js-hw-11',
+    demo_url: 'https://dominikbanaskiewicz.github.io/goit-js-hw-11/',
+    technologies: ['javascript', 'restApi', 'sass', 'axios'],
     descriptin:
       'Me and five other peoples write this site. Crating this site i have learned how to use git with others people, how to choose the best way and how to work with someone else code',
   },
